b8b18ddae7771f91e37fc049720f1461495bb7e1
renfei <renfei-hj@huafang.com>
2023-10-19 18:49:32 +0800

    更新代理

--- Dockerfile
+++ Dockerfile
@@ -2,7 +2,7 @@ FROM alpine:latest
 
 RUN sed -i 's/dl-cdn.alpinelinux.org/mirrors.aliyun.com/g' /etc/apk/repositories
 
-RUN apk add --no-cache tzdata ca-certificates && rm -rf /var/cache/apk/*
+#RUN apk add --no-cache tzdata ca-certificates && rm -rf /var/cache/apk/*
 
 RUN mkdir -p /data/deploy
 
@@ -15,4 +15,7 @@ EXPOSE 9958
 
 WORKDIR /data/deploy
 
-CMD ["sh", "-c", "./proxy -m=proxy -p=0.0.0.0:9958"]
+CMD ["sh", "-c", "./run.sh"]
+
+#docker run --rm -it -p 9958:9958 -e secret= -v ./run.sh:/data/deploy/run.sh  registry.cn-hangzhou.aliyuncs.com/vary/lnmp:go-proxy sh
+#docker run --rm -it  -e secret= registry.cn-hangzhou.aliyuncs.com/vary/lnmp:go-proxy sh
---
+++ run.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+if [ "${secret}" == "" ];then
+  echo "需要secret 环境变量"
+  exit
+fi
+if [ "${app}" == "" ];then
+  app="cpolar_proxy"
+  proxy="proxy"
+fi
+
+function run_cpolar(){
+	ps -ef |grep cpolar: |grep -v 'grep' |awk '{print $2}' |xargs kill -9
+
+	rm -rf ./log*
+	./$app authtoken $secret
+	./$app tcp 9958 -log ./log --log-level INFO --daemon on &
+	proxyAddr=$(get_url)
+	echo $proxyAddr
+	run_proxy $proxyAddr
+}
+
+function get_url(){
+	sleep 3
+	tcp=$(tail -20 ./log |grep 'Tunnel established' |cut -d " " -f 9|sed "s/\"//g")
+	echo $tcp >>./log
+	echo $tcp
+}
+
+function run_proxy() {
+  ps -ef |grep 9958 |grep -v 'grep' |awk '{print $2}' |xargs kill -9
+  nohup ./$proxy -m=proxy -p=0.0.0.0:9958 r=$1 >./log 2>&1 &
+  tail ./log
+}
+
+run_cpolar
